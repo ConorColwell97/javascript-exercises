1f64df35e9199387c834cb121ba50e8acd75e8ef
ConorColwell97 <conormcolwell@gmail.com>
2025-07-02 23:03:43 +0100

    Completed exercises 6 - 11

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    let result = arr.filter((item) => {
+        let seen = false;
+        let i = 0;
+
+        while(i < args.length && !seen) {
+            if(item === args[i++]) {
+                seen = true;
+            }
+        }
+
+        if(!seen) {
+            return item;
+        }
+    });
+    return result;
 };
 
 // Do not edit below this line
--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if(typeof start != "number" || typeof end != "number" || start < 0 || end < 0 || !Number.isInteger(start) || !Number.isInteger(end)) {
+        return 'ERROR';
+    }
 
+    let sum = 0;
+    if(start <= end) {
+        for(let n = start; n <= end; n++) {
+            sum += n;
+        }
+    } else {
+        for(let n = start; n >= end; n--) {
+            sum += n;
+        }
+    }
+
+    return sum;
 };
+
+console.log(sumAll(2,4))
 
 // Do not edit below this line
 module.exports = sumAll;
